cfcbd7bc04158077e27db8ab47f33b966a0a152f
CyB3RTYp3 <rohanmanopkt@gmail.com>
2023-10-29 15:20:20 +0530

    added back button to empProfile

---
+++ src/Components/Coming_Soon/Coming.jsx
@@ -0,0 +1,9 @@
+import styles from './Coming.module.css'
+
+export default function Coming() {
+  return (
+    <div className={styles.Coming}>
+      <h1>Coming Soon ........</h1> 
+    </div>
+  )
+}
